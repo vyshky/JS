8214c0081a4ecb2e891a7303263b023bda549773
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2022-07-30 22:13:05 +0400

    5 дз доне

---
+++ 5/script.js
@@ -0,0 +1,32 @@
+let str = prompt("Введите математическое выражение");
+console.log(str);
+
+let operatorA = parseInt(str);
+let operatorB = parseInt(str.slice(+operatorA.toString().length + 1));
+
+let lengthA = operatorA.toString().length;
+let lengthB = operatorB.toString().length;
+
+let operation = str[lengthA];
+
+if (str.length == lengthA + lengthB + 1) {
+  console.log("Операция " + operation);
+  switch (operation) {
+    case "+":
+      print(+operatorA + operatorB);
+      break;
+    case "-":
+      print(+operatorA - operatorB);
+      break;
+    case "*":
+      print(+operatorA * operatorB);
+      break;
+    case "/":
+      print(+operatorA / operatorB);
+      break;
+  }
+}
+
+function print(_expression) {
+  console.log("Ответ " + _expression);
+}
